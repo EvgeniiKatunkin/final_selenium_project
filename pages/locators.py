9890d38ff155e7ded638ee6b9c02143dcec13fdd
Evgenii <evgeniikatunkin@gmail.com>
2023-01-31 22:57:04 +0100

    base_page.py: 2 methods for login button are added locators.py: BasePageLocators are added. Only login_link for now. main_page.py: is empty because it's methods are in base_page.py since now. test_product_page.py: 2 tests for login link are added.

--- pages/locators.py
+++ pages/locators.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class ProductPageLocators:
